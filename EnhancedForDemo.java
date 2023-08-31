73ab0db7a26c4030fab0b89c5093c49026c64135
AfroGit <17697323-AfroGit@users.noreply.replit.com>
2023-08-31 20:46:22 +0000

    variables declared in a loop initiator have scope only in the loop block.
    Notice how the code declares a variable within the initialization expression. The scope of this variable extends from its declaration to the end of the block governed by the for statement, so it can be used in the termination and increment expressions as well. If the variable that controls a for statement is not needed outside of the loop, it's best to declare the variable in the initialization expression. The names i, j, and k are often used to control for loops; declaring them within the initialization expression limits their life span and reduces errors.

--- EnhancedForDemo.java
+++ EnhancedForDemo.java
@@ -3,6 +3,8 @@ public class EnhancedForDemo {
          int[] numbers = 
              {1,2,3,4,5,6,7,8,9,10};
          for (int item : numbers) {
+
+           // the variable item holds the current value from the numbers array
              System.out.println("Count is: " + item);
          }
     }
--- ForDemo.java
+++ ForDemo.java
@@ -3,3 +3,14 @@
 * # Date: 8/31/2023
 * # Mission: To learn the basics of a for loop,its syntax,why and all its hows
 */ 
+
+public class ForDemo {
+    public static void main(String[] args){
+         for(int i=0; i<11; i++){
+           // initialization expression initializes the loop; it's executed once, as the loop begins.
+           // When the termination expression evaluates to false, the loop terminates.
+           // increment expression is invoked after each iteration through the loop
+              System.out.println("Count is: " + i);
+         }
+    }
+}
